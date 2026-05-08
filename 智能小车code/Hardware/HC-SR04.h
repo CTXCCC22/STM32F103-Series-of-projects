c22e77d78452aa43f865e1ba6c5313ad347e8449
CTXCCC22 <2052985452@qq.com>
2026-05-08 14:18:20 +0800

    Delete 智能小车code directory

--- 智能小车code/Hardware/HC-SR04.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HC_SR04_H
-#define __HC_SR04_H
-
-#include "stm32f10x.h"
-
-void Ultrasound_Init(void);
-float Test_Distance(void);
-
-#endif
--- 智能小车code/Hardware/hongwai.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TRACKER_H
-#define __TRACKER_H
-
-#include "stm32f10x.h"
-
-void Tracker_Init(void);
-uint8_t Tracker_Read(void);
-
-#endif
